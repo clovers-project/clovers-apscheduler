64c2ca8621c1774652462ef2e068349a423633ad
KarisAya <1048827424@qq.com>
2025-01-10 00:17:50 +0800

    删除pydantic依赖

--- clovers_apscheduler/__init__.py
+++ clovers_apscheduler/__init__.py
@@ -1,28 +1,37 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from clovers.core.config import config as clovers_config
-from clovers.core.plugin import Plugin
-from .config import Config
+from clovers import Plugin
+from clovers.logger import logger
+from clovers.config import config as clovers_config
 
+scheduler_config: dict = {"apscheduler.timezone": "Asia/Shanghai"}
 
 config_key = __package__
-config_data = Config.model_validate(clovers_config.get(config_key, {}))
-clovers_config[config_key] = config_data.model_dump()
+config_data = clovers_config.get(config_key, scheduler_config)
+clovers_config[config_key] = config_data
+scheduler_config.update(config_data)
+
 
 plugin = Plugin()
 scheduler = AsyncIOScheduler()
-scheduler.configure(**config_data.scheduler_config)
+scheduler.configure(**scheduler_config)
 
 
 @plugin.startup
 async def _():
     if not scheduler.running:
+        logger.debug("Starting scheduler")
         scheduler.start()
+    else:
+        logger.debug("Scheduler already running")
 
 
 @plugin.shutdown
 async def _():
     if scheduler.running:
+        logger.debug("Shutting down scheduler")
         scheduler.shutdown()
+    else:
+        logger.debug("Scheduler not running")
 
 
 __plugin__ = plugin
--- clovers_apscheduler/config.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Config(BaseModel):
-    scheduler_config: dict = {"apscheduler.timezone": "Asia/Shanghai"}
